b64ab200a636e12cbee7e94c16a414318f8a6587
Abhishek069 <abhishinde0723@gmail.com>
2023-10-29 11:48:29 +0000

    commit message

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import GamePlay from './componets/GamePlay'
+import './App.css'
+
+// const choicesList = [
+//   {
+//     id: 'ROCK',
+//     imageUrl:
+//       'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+//   },
+//   {
+//     id: 'SCISSORS',
+//     imageUrl:
+//       'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+//   },
+//   {
+//     id: 'PAPER',
+//     imageUrl:
+//       'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+//   },
+// ]
+
+const App = () => (
+  <div>
+    <GamePlay />
+  </div>
+)
+
+export default App
